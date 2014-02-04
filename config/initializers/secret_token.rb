dbd6dc7c2b8a9f1ca0604bdad45534e17c3888d1
taimoor <taimoor.changaiz@coeus-solutions.de>
2014-02-04 19:00:52 +0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeliverIt::Application.config.secret_token = '6b8900253d9fbac40bd4cd47ca6c3903265146ca89a219c22c25a04f4621f5f2f866b798f148dc3c4fdd88bff36111b17db6a5426120c4d1355fb59e84f0b12e'
